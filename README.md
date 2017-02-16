d2dace835d1d137df7648883110468ba52ac3869
Binit Shah <bshah@gatech.edu>
2017-02-16 03:02:33 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# waterwhere
-CS2340 Project
+# Waterwhere
+Waterwhere is GitAtMe's CS2340 Project for the 2017 Spring Semester.
+
+## Status
+Currently working on M4 which is concerned with implementing the login screens.
+
+## GitAtMe Members
+* Aditya Nadkarni
+* Binit Shah
+* Shukan Shah
+* Vishwa Shah
+* Yatharth Dubey
---
+++ app/src/main/java/com/gitatme/waterwhere/MainActivity.java
@@ -0,0 +1,58 @@
+package com.gitatme.waterwhere;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        //Check if user is logged in, and if not, send them to Onboarding
+        SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.shared_pref_code), Context.MODE_PRIVATE);
+        boolean isLoggedIn = sharedPreferences.getBoolean(getString(R.string.shared_pref_loggedin), false);
+        if(!isLoggedIn) {
+            Intent intent = new Intent(this, OnboardingActivity.class);
+            startActivity(intent);
+            //finish();
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
