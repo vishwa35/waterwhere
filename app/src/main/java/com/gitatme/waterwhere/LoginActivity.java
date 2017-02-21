255b25406f77072ce4c821c48fb6aacbb95cebae
Shukan Shah <shukanshah14@gmail.com>
2017-02-21 18:32:48 -0500

    Added user credential validation functionality

--- app/src/main/java/com/gitatme/waterwhere/LoginActivity.java
+++ app/src/main/java/com/gitatme/waterwhere/LoginActivity.java
@@ -1,18 +1,59 @@
 package com.gitatme.waterwhere;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private final String USERNAME = "user"; //hardcoded for now
+    private final String PASSWORD = "pass"; //hardcoded for now
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        usernameEditText = (EditText) findViewById(R.id.usernameEditText);
+        passwordEditText = (EditText) findViewById(R.id.passwordEditText);
     }
 
     public void onClickLogin(View view) {
+        if (usernameEditText.getText().toString().trim().isEmpty()
+                && passwordEditText.getText().toString().isEmpty())  {
+            Toast.makeText(this, "You did not enter a username or password",
+                    Toast.LENGTH_SHORT).show();
+        } else if (usernameEditText.getText().toString().trim().isEmpty()) {
+            Toast.makeText(this, "You did not enter a username",
+                    Toast.LENGTH_SHORT).show();
+        } else if (passwordEditText.getText().toString().trim().isEmpty()) {
+            Toast.makeText(this, "You did not enter a password",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            if (usernameEditText.getText().toString().equals(USERNAME)
+                    && passwordEditText.getText().toString().equals(PASSWORD)) {
+                Intent loginSuccessful = new Intent(this,
+                        MainActivity.class);
+                startActivity(loginSuccessful);
+            } else {
+                new AlertDialog.Builder(this)
+                        .setTitle("Invalid Credentials")
+                        .setMessage("The username or password you entered was incorrect")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // do nothing
+                            }
+                        })
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+            }
+        }
     }
 
     public void onCancelClick(View view) {
