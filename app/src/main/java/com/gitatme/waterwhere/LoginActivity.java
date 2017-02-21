1670c8705638a6619b619a9dd856b57ab3193528
Shukan Shah <shukanshah14@gmail.com>
2017-02-21 16:26:43 -0500

    Added a cancel button and edited login activity code

--- app/src/main/java/com/gitatme/waterwhere/LoginActivity.java
+++ app/src/main/java/com/gitatme/waterwhere/LoginActivity.java
@@ -2,6 +2,7 @@ package com.gitatme.waterwhere;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onClickLogin(View view) {
+    }
+
+    public void onCancelClick(View view) {
     }
 }
