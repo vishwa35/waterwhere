65777629857ad90e53a7ab5d0bb71a4f03f1d58d
Binit Shah <bshah@gatech.edu>
2017-03-01 01:54:51 -0500

    fixing onboarding buttons

---
+++ app/src/main/java/com/gitatme/waterwhere/OnboardingFragment.java
@@ -0,0 +1,37 @@
+package com.gitatme.waterwhere;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class OnboardingFragment extends Fragment {
+
+
+    int position;
+    RelativeLayout relativeLayout;
+    Button continueButton;
+
+    public OnboardingFragment() {}
+
+    public void newInstance(int position){
+        this.position = position;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_onboarding, container, false);
+    }
+
+}
