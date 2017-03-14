82a6e0691bcc1f643e70b9cf3eeffdccf5039071
Shukan Shah <shukanshah14@gmail.com>
2017-03-14 18:55:07 -0400

    changes to main activity

--- app/src/main/java/com/gitatme/waterwhere/view/ViewWaterReportActivity.java
+++ app/src/main/java/com/gitatme/waterwhere/view/ViewWaterReportActivity.java
@@ -1,13 +1,18 @@
 package com.gitatme.waterwhere.view;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 import com.gitatme.waterwhere.R;
+import com.gitatme.waterwhere.model.WaterReport;
+import com.google.gson.Gson;
 
 public class ViewWaterReportActivity extends AppCompatActivity {
 
@@ -15,17 +20,15 @@ public class ViewWaterReportActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_water_report);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        Gson gson = new Gson();
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        String json1 = sharedPreferences.getString("waterReport", "");
+        WaterReport obj1 = gson.fromJson(json1, WaterReport.class);
+//        Toast.makeText(this, obj1.toString(), Toast.LENGTH_SHORT).show();
+
+        TextView reportText = (TextView)findViewById(R.id.existingReport);
+        reportText.setText(obj1.toString());
     }
 
 }
